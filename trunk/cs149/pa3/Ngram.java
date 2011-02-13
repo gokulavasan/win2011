1d14130c755226aeb558583d9aae2d259597c6c9
joshlang <joshlang@d2296c49-649c-edf8-8ecd-71de0921cff7>
2011-02-13 04:32:36 +0000

    Replaced ngram with version 2 wordcount and updated the argument lists

--- trunk/cs149/pa3/Ngram.java
+++ trunk/cs149/pa3/Ngram.java
@@ -1,7 +1,8 @@
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.conf.*;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
@@ -13,16 +14,70 @@ public class Ngram extends Configured implements Tool {
 
    public static class Map
        extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+     static enum Counters { INPUT_WORDS }
+
      private final static IntWritable one = new IntWritable(1);
      private Text word = new Text();
 
+     private boolean caseSensitive = true;
+     private Set<String> patternsToSkip = new HashSet<String>();
+
+     private long numRecords = 0;
+     private String inputFile;
+
+     public void setup(Context context) {
+       Configuration conf = context.getConfiguration();
+       caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
+       inputFile = conf.get("mapreduce.map.input.file");
+
+       if (conf.getBoolean("wordcount.skip.patterns", false)) {
+         Path[] patternsFiles = new Path[0];
+         try {
+           patternsFiles = DistributedCache.getLocalCacheFiles(conf);
+         } catch (IOException ioe) {
+           System.err.println("Caught exception while getting cached files: "
+               + StringUtils.stringifyException(ioe));
+         }
+         for (Path patternsFile : patternsFiles) {
+           parseSkipFile(patternsFile);
+         }
+       }
+     }
+
+     private void parseSkipFile(Path patternsFile) {
+       try {
+         BufferedReader fis = new BufferedReader(new FileReader(
+             patternsFile.toString()));
+         String pattern = null;
+         while ((pattern = fis.readLine()) != null) {
+           patternsToSkip.add(pattern);
+         }
+       } catch (IOException ioe) {
+         System.err.println("Caught exception while parsing the cached file '"
+             + patternsFile + "' : " + StringUtils.stringifyException(ioe));
+       }
+     }
+
      public void map(LongWritable key, Text value, Context context)
          throws IOException, InterruptedException {
-       String line = value.toString();
+       String line = (caseSensitive) ?
+           value.toString() : value.toString().toLowerCase();
+
+       for (String pattern : patternsToSkip) {
+         line = line.replaceAll(pattern, "");
+       }
+
        StringTokenizer tokenizer = new StringTokenizer(line);
        while (tokenizer.hasMoreTokens()) {
          word.set(tokenizer.nextToken());
          context.write(word, one);
+         context.getCounter(Counters.INPUT_WORDS).increment(1);
+       }
+
+       if ((++numRecords % 100) == 0) {
+         context.setStatus("Finished processing " + numRecords
+             + " records " + "from the input file: " + inputFile);
        }
      }
    }
@@ -40,7 +95,8 @@ public class Ngram extends Configured implements Tool {
      }
    }
 
-   public int run(String [] args) throws Exception {
+   public int run(String[] args) throws Exception {
+     //Args format: <#Ngram> <query_file> <input_dir> <output_dir>
      Job job = new Job(getConf());
      job.setJarByClass(Ngram.class);
      job.setJobName("Ngram");
@@ -52,20 +108,25 @@ public class Ngram extends Configured implements Tool {
      job.setCombinerClass(Reduce.class);
      job.setReducerClass(Reduce.class);
 
+     // Note that these are the default.
      job.setInputFormatClass(TextInputFormat.class);
      job.setOutputFormatClass(TextOutputFormat.class);
+
+     int ngram_num = Integer.parseInt(args[0]);
      
-     Path query = new Path (args[0]);
-     FileInputFormat.setInputPaths(job, new Path(args[1]));
-     FileOutputFormat.setOutputPath(job, new Path(args[2]));
+     DistributedCache.addCacheFile(new Path(args[1]).toUri(),
+             job.getConfiguration());
+     //job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
+
+     FileInputFormat.setInputPaths(job, new Path(args.get(2)));
+     FileOutputFormat.setOutputPath(job, new Path(args.get(3)));
 
      boolean success = job.waitForCompletion(true);
      return success ? 0 : 1;
    }
 
    public static void main(String[] args) throws Exception {
-     int ret = ToolRunner.run(new Ngram(), args);
-     System.exit(ret);
+     int res = ToolRunner.run(new Configuration(), new Ngram(), args);
+     System.exit(res);
    }
 }
-
