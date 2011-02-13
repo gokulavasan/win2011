5d16771d1bcdf160fc8bf5e42b64421e070401e8
joshlang <joshlang@d2296c49-649c-edf8-8ecd-71de0921cff7>
2011-02-13 03:40:16 +0000

    Moved WordCount into Ngram shell

---
+++ trunk/cs149/pa3/Ngram.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.*;
+
+public class Ngram extends Configured implements Tool {
+
+   public static class Map
+       extends Mapper<LongWritable, Text, Text, IntWritable> {
+     private final static IntWritable one = new IntWritable(1);
+     private Text word = new Text();
+
+     public void map(LongWritable key, Text value, Context context)
+         throws IOException, InterruptedException {
+       String line = value.toString();
+       StringTokenizer tokenizer = new StringTokenizer(line);
+       while (tokenizer.hasMoreTokens()) {
+         word.set(tokenizer.nextToken());
+         context.write(word, one);
+       }
+     }
+   }
+
+   public static class Reduce
+       extends Reducer<Text, IntWritable, Text, IntWritable> {
+     public void reduce(Text key, Iterable<IntWritable> values,
+         Context context) throws IOException, InterruptedException {
+
+       int sum = 0;
+       for (IntWritable val : values) {
+         sum += val.get();
+       }
+       context.write(key, new IntWritable(sum));
+     }
+   }
+
+   public int run(String [] args) throws Exception {
+     Job job = new Job(getConf());
+     job.setJarByClass(Ngram.class);
+     job.setJobName("wordcount");
+
+     job.setOutputKeyClass(Text.class);
+     job.setOutputValueClass(IntWritable.class);
+
+     job.setMapperClass(Map.class);
+     job.setCombinerClass(Reduce.class);
+     job.setReducerClass(Reduce.class);
+
+     job.setInputFormatClass(TextInputFormat.class);
+     job.setOutputFormatClass(TextOutputFormat.class);
+     
+     Path query = new Path (args[0]);
+     FileInputFormat.setInputPaths(job, new Path(args[1]));
+     FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+     boolean success = job.waitForCompletion(true);
+     return success ? 0 : 1;
+   }
+
+   public static void main(String[] args) throws Exception {
+     int ret = ToolRunner.run(new Ngram(), args);
+     System.exit(ret);
+   }
+}
+
