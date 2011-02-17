67e83f6339fabcafd541b0215a9a51cc1a9d9170
gokulavasan <gokulavasan@d2296c49-649c-edf8-8ecd-71de0921cff7>
2011-02-17 06:39:42 +0000

    Initial Code for NullCheck Analysis

--- cs243/hw4/optimize/src/optimize/FindRedundantNullChecks.java
+++ cs243/hw4/optimize/src/optimize/FindRedundantNullChecks.java
@@ -10,7 +10,18 @@ public class FindRedundantNullChecks {
      * method should print out a list of quad ids of redundant null checks
      * for each function as described on the course webpage     
      */
+    
+     
     public static void main(String[] args) {	        
         //fill me in
+	int i;
+	jq_Class[] classes = new jq_Class[args.length];
+	for (i = 0; i < args.length; i++)
+	{
+		ReferenceSolver nullSolve = new ReferenceSolver();
+		nullSolve.registerAnalysis(new NullCheckAnalysis());
+		Helper.runPass(classes[i], nullSolve);
+		System.out.println(args[i]);	
+	}
     }
 }
